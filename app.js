d760a7f193a3797f039d81099542aa11168cf7ea
zflopez <zaida.flopez@gmail.com>
2016-08-24 13:47:51 +0200

    html and js files set up

---
+++ app.js
@@ -0,0 +1,13 @@
+new Vue({
+	// Targets the div with id 'events'
+	el: '#events',
+
+	// Registers all the ViewModel data
+	data: {},
+
+	// Loads everything within function when app loads
+	ready: function() {},
+
+	// Registers custom methods for the app
+	methods: {}
+});
